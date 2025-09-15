3aa7bccd566a5a48538c6097757bbe658a222e46
Erik Sorensen <erik.sorensen2006@gmail.com>
2025-09-15 16:47:36 -0400

    Added web console to override console class for web interpretation

---
+++ VirtualRouter/src/web/WebConsole.hpp
@@ -0,0 +1,123 @@
+// WebConsole.hpp
+
+#ifndef WEB_CONSOLE_HPP
+#define WEB_CONSOLE_HPP
+
+#include <Console.h>
+#include <CliSession.h>
+#include "UnixApi.h"
+#include <json.hpp>
+
+#include <unordered_map>
+#include <sstream>
+#include <mutex>
+
+// Mock class for IConsole using Google Mock
+class WebConsole : public IConsole
+{
+public:
+    WebConsole(UnixApi& api, int clientFd)
+        : api(api), clientFd(clientFd) {}
+
+    void clearScreen() override {}
+    void enableLineWrapping() override {}
+    void clearLineAfterCursor() override {}
+    void saveCursorPosition() override {}
+    void restoreCursorPosition() override {}
+    void moveCursorToStart() override {}
+    void moveCursorLeft(size_t count) override {}
+    void moveCursorRight(size_t count) override {}
+    void moveCursorUp(size_t count) override {}
+    void moveCursorDown(size_t count) override {}
+    size_t getTerminalWidth() override { return std::numeric_limits<size_t>::max(); }
+    void beep() override {}
+    CursorPosition getCursorPosition() override { return CursorPosition{}; }
+
+    bool getPrompt(std::string& p)
+    {
+        p = prompt;
+        if (prompt.empty()) return false;
+        prompt.clear();
+        return true;
+    }
+
+    void print(const std::string& str, Color color) override
+    {
+        if (color == Color::PROMPT) // Secret prompt option
+        {
+            prompt = str;
+            std::cout << str;
+            return;
+        }
+
+        std::string colored = applyColor(str, color);
+        {
+            std::lock_guard<std::mutex> lock(mu);
+            buffer << colored;
+        }
+
+        std::cout << colored;
+    }
+
+    void beginSession()
+    {
+        std::lock_guard<std::mutex> lock(mu);
+        buffer.str("");
+        buffer.clear();
+    }
+
+    void flushCommands(CliSession& endSequence, bool scroll = false)
+    {
+        std::string output;
+        {
+            std::lock_guard<std::mutex> lock(mu);
+            if (buffer.str().empty()) return;
+            output = buffer.str();
+            buffer.str("");
+            buffer.clear();
+        }
+        auto reply = [&]() -> nlohmann::json {
+            nlohmann::json j = {
+                {"action", "cmd"},
+                {"type", "router"},
+                {"data", output},
+                {"cursor_pos", endSequence.getInputCursorPosition() },
+                {"initial_length", endSequence.getInitialLineLength() }
+            };
+            
+            std::string prompt;
+            if (getPrompt(prompt))
+                j["prompt"] = prompt;
+            if (scroll)
+                j["scroll"] = scroll;
+            
+            return j;
+        };
+        api.sendJson(clientFd, reply());
+    }
+
+private:
+    UnixApi& api;
+    int clientFd;
+    std::mutex mu;
+    std::stringstream buffer;
+
+    std::string prompt;
+
+    static std::string applyColor(const std::string& s, Color c) {
+        switch (c) {
+            case Color::BLACK:   return "\033[1;30m" + s + "\033[0m";
+            case Color::RED:     return "\033[1;31m" + s + "\033[0m";
+            case Color::GREEN:   return "\033[1;32m" + s + "\033[0m";
+            case Color::YELLOW:  return "\033[1;33m" + s + "\033[0m";
+            case Color::BLUE:    return "\033[1;34m" + s + "\033[0m";
+            case Color::MAGENTA: return "\033[1;35m" + s + "\033[0m";
+            case Color::CYAN:    return "\033[1;36m" + s + "\033[0m";
+            case Color::WHITE:   return s + "\033[0m";
+            case Color::NONE:    return s;
+            default:             return s;
+        }
+    }
+};
+
+#endif // MOCK_CONSOLE_HPP
