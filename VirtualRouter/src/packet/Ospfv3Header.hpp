08b9302b48b3eba010f2e3f7c9c85c52a9413c17
Erik Sorensen <erik.sorensen2006@gmail.com>
2025-12-18 13:53:54 -0500

    Added OSPF headers

---
+++ VirtualRouter/src/packet/Ospfv3Header.hpp
@@ -0,0 +1,63 @@
+// Ospfv3Header.hpp
+
+#ifndef OSPFV3_HEADER_HPP
+#define OSPFV3_HEADER_HPP
+
+#include <HeaderHelpers.hpp>
+#include <TlvOptions.hpp>
+
+#define OSPFV3_VERSION 3 ///< OSPFv2 Version (2).
+
+#define OSPFV3_TYPE_HELLO                   0x01 ///< OSPFv3 Hello Type (1).
+#define OSPFV3_TYPE_DATABASE_DESCRIPTION    0x02 ///< OSPFv3 Database Description Type (2).
+#define OSPFV3_TYPE_LINK_STATE_REQUEST      0x03 ///< OSPFv3 Link State Request Type (3).
+#define OSPFV3_TYPE_LINK_STATE_UPDATE       0x04 ///< OSPFv3 Link State Update Type (4).
+#define OSPFV3_TYPE_LINK_STATE_ACK          0x05 ///< OSPFv3 Link State Acknowledgment (5).
+
+/**
+ * @struct Ospfv3HeaderRaw
+ */
+#pragma pack(push, 1) 
+struct Ospfv3HeaderRaw
+{
+    uint8_t version;
+    uint8_t type;
+    uint8_t packetLength[2];
+    uint8_t routerID[4];
+    uint8_t areaID[4];
+    uint8_t checksum[2];
+    uint8_t instanceID;
+    uint8_t reserved;
+};
+#pragma pack(pop)
+
+/**
+ * @struct Ospfv3Header
+ * @brief Represents an OSPF (Open Shortest Path First) header.
+ */
+struct Ospfv3Header
+{
+    DEFINE_FIXED_HEADER(Ospfv3HeaderRaw);
+
+    uint8_t getVersion() const              { return raw->version; }
+    uint8_t getType() const                 { return raw->type; }
+    uint16_t getPacketLen() const           { return readU16(raw->packetLength); }
+    uint32_t getRouterID() const            { return readU32(raw->routerID); }
+    uint32_t getAreaID() const              { return readU32(raw->areaID); }
+    uint8_t getInstanceID() const           { return raw->instanceID; }
+
+    void setVersion(uint8_t val)
+        { raw->version = val; }
+    void setType(uint8_t val)
+        { raw->type = val; }
+    void setPacketLen(uint16_t val)
+        { writeU16(raw->packetLength, val); }
+    void setRouterID(uint32_t val)
+        { writeU32(raw->routerID, val); }
+    void setAreaID(uint32_t val)
+        { writeU32(raw->areaID, val); }
+    void setInstanceID(uint8_t val)
+        { raw->instanceID = val; }
+};
+
+#endif // OSPFV3_HEADER_HPP
