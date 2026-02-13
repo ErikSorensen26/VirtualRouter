86854c10d3483b11c82ada6fb820a3b5c0fed274
Erik Sorensen <erik.sorensen2006@gmail.com>
2026-02-12 20:29:12 -0500

    registry fix for disabling context index

--- VirtualRouter/src/configs/SubRegistry.hpp
+++ VirtualRouter/src/configs/SubRegistry.hpp
@@ -186,6 +186,7 @@ public:
 
     static_assert(sizeof...(Fields) == Config::toIndex<ENUM::COUNT>);
 
+#if USE_CONFIG_INDEX
     static_assert(
         []<size_t... Is>(std::index_sequence<Is...>) constexpr
         {
@@ -197,6 +198,7 @@ public:
         }(std::make_index_sequence<std::tuple_size_v<FieldTuple>>{}),
         "Tuple order must match enum field indicies"
     );
+#endif
 
     explicit SimpleSubRegistry() noexcept
         : fields(),
